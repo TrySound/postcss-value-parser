7a14e6c45c90e3878cea2c4a07697be544c99942
Bogdan Chadkin <trysound@yandex.ru>
2015-10-03 15:10:11 +0300

    New API

--- test/unit.js
+++ test/unit.js
@@ -1,37 +1,36 @@
 var test = require('tape');
 var unit = require('../lib/unit');
 
+var tests = [{
+    fixture: '.23rem',
+    expected: { number: '.23', unit: 'rem' }
+}, {
+    fixture: '.2.3rem',
+    expected: { number: '.2', unit: '.3rem' }
+}, {
+    fixture: '2.',
+    expected: { number: '2.', unit: '' }
+}, {
+    fixture: '+2.',
+    expected: { number: '+2.', unit: '' }
+}, {
+    fixture: '-2.',
+    expected: { number: '-2.', unit: '' }
+}, {
+    fixture: '+-2.',
+    expected: false
+}, {
+    fixture: '.',
+    expected: false
+}, {
+    fixture: '.rem',
+    expected: false
+}];
+
 test('Unit', function (t) {
-    t.plan(8);
+    t.plan(tests.length);
 
-    t.deepEqual(unit('.23rem'), {
-        number: '.23',
-        unit: 'rem'
+    tests.forEach(function (test) {
+        t.deepEqual(unit(test.fixture), test.expected);
     });
-
-    t.deepEqual(unit('.2.3rem'), {
-        number: '.2',
-        unit: '.3rem'
-    });
-
-    t.deepEqual(unit('2.'), {
-        number: '2.',
-        unit: ''
-    });
-
-    t.deepEqual(unit('+2.'), {
-        number: '+2.',
-        unit: ''
-    });
-
-    t.deepEqual(unit('-2.'), {
-        number: '-2.',
-        unit: ''
-    });
-
-    t.equal(unit('+-2.'), false);
-
-    t.equal(unit('.'), false);
-
-    t.equal(unit('.rem'), false);
 });
